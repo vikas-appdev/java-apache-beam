f1f353106d38e657e73a20701836682698550617
Vikas Yadav <v.ray96610@gmail.com>
2024-04-25 11:49:19 +0530

    Project init with Spring boot

---
+++ src/test/java/com/gradlic/beam/beamdemo/BeamdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradlic.beam.beamdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeamdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
